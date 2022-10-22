25092aa8b943d6c8f798971825bb8a2d103578db
danerini2019 <danerini@gmail.com>
2022-10-21 23:52:22 -0700

    proof of concept for one team parse

--- walkup_parse.py
+++ walkup_parse.py
@@ -1,25 +1,43 @@
 import requests
 from bs4 import BeautifulSoup
+import pandas as pd
 
-page = requests.get("https://www.mlb.com/phillies/ballpark/music")
+pd.set_option('display.max_colwidth', None)
+
+page = requests.get('https://www.mlb.com/phillies/ballpark/music')
 soup = BeautifulSoup(page.content, 'html.parser')
 
 stuff = list(soup.children)[1]
 lst = [type(item) for item in list(soup.children)]
-# print(lst)
 stuff = list(stuff.children)[1]
-# print(stuff.prettify())
-player_names = stuff.find_all(class_="u-text-h4 u-text-flow")
-# print(player_names.get_text())
-player_songs = stuff.find_all(class_="u-app-show")
-# print(player_songs.get_text())
+player_names = stuff.find_all(class_='u-text-h4 u-text-flow')
+player_songs = stuff.find_all(class_='u-app-show')
 
-for i in range(len(player_names)):    
-    print(player_names[i].get_text())
+# lists of songs and players
+player_lst = [player_names[i].get_text() for i in range(len(player_names))]
+song_lst = [player_songs[i].get_text() for i in range(len(player_songs))]
 
-for i in range(len(player_songs)):    
-    print(player_songs[i].get_text())
+# Bringing values into dataframe and adding rows for players with multiple songs
+for i in range(len(song_lst)):
+    song_lst[i] = song_lst[i].strip()
 
-# Players with multiple songs are separated by returns in 
-# the text. Need to pull with conditional statements to
-# frame each player
+dict = {'players': player_lst,'songs': song_lst}
+
+df = pd.DataFrame(columns = ['players', 'songs'])
+
+for i in range(len(dict['songs'])):
+
+    if '\n\n' in dict['songs'][i]:
+        plr_song_lst = dict['songs'][i].split('\n\n')
+
+        for j in range(len(plr_song_lst)):
+            append_row_dict = {'players': [dict['players'][i]], 'songs': [plr_song_lst[j]]}
+            append_row = pd.DataFrame(append_row_dict)
+            df.loc[len(df.index)] = append_row.loc[0]
+        
+    else:
+        append_row_dict = {'players': [dict['players'][i]], 'songs': [dict['songs'][i]]}
+        append_row = pd.DataFrame(append_row_dict)
+        df.loc[len(df.index)] = append_row.loc[0]
+        
+print(df)
